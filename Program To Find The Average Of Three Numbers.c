c39ab1eeb49344b600d4ca92c9ee73c977d393e5
R Raksha <154651032+Rakshar30@users.noreply.github.com>
2025-06-18 21:57:03 +0530

    Create Program To Find The Average Of Three Numbers.c

---
+++ Program To Find The Average Of Three Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+   int x, y, z;           
+   float avg;
+   printf("Enter Three Numbers : \n");    
+   scanf("%d %d %d",&x, &y, &z);  
+ 
+   avg=(x+y+z)/3.0;                 
+   printf("Average of Three Numebers is : %f", avg);    
+   
+}
